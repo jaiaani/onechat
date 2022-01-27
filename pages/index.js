ab6e17b0596e106464676fd00defffcbb2e197f5
Jaiane Guimarães <dsjaiane.guimaraes@gmail.com>
2022-01-27 11:48:10 -0300

    att aula 2/ adicionando pesquisa por user

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,9 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
-import appConfig from '../config.json';
+import appConfig from '../config.json'
+import React from 'react'
+import {useRouter} from'next/router'
 
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: 'Open Sans', sans-serif;
-      }
-      /* App fit Height */ 
-      html, body, #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */ 
-    `}</style>
-  );
-}
+
 
 function Titulo(props) {
   const Tag = props.tag || 'h1';
@@ -49,11 +24,12 @@ function Titulo(props) {
 
 
 export default function PaginaInicial() {
-  const username = 'jaiaani';
-
+  const [username, setUsername] = React.useState('jaiaani')
+  const roteamento = useRouter()
+  
   return (
     <>
-      <GlobalStyle />
+     
       <Box
         styleSheet={{
           display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -74,12 +50,17 @@ export default function PaginaInicial() {
             width: '100%', maxWidth: '700px',
             borderRadius: '5px', padding: '32px', margin: '16px',
             boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
-            backgroundColor: appConfig.theme.colors.onePiece['azulClaro'],
+            backgroundColor: appConfig.theme.colors.onePiece['azulClaroTransparente'],
           }}
         >
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit = {function (infosDoEvento){
+              infosDoEvento.preventDefault()
+              console.log('aaaaaaaaaaaaaa')
+              roteamento.push('/chat')
+            }}
             styleSheet={{
               display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
               width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -90,7 +71,15 @@ export default function PaginaInicial() {
               {appConfig.name} {username}
             </Text>
 
-            <TextField
+          
+
+       <TextField value={username}
+
+          onChange={function (event){
+              console.log('usuario digitou', event.target.value)
+              const valor = event.target.value
+              setUsername(valor);
+            }}
               fullWidth
               textFieldColors={{
                 neutral: {
@@ -124,9 +113,9 @@ export default function PaginaInicial() {
               alignItems: 'center',
               maxWidth: '200px',
               padding: '16px',
-              backgroundColor: appConfig.theme.colors.onePiece["azulEscuro"],
+              backgroundColor: appConfig.theme.colors.onePiece["azulEscuroTransparente"],
               border: '1px solid',
-              borderColor: appConfig.theme.colors.onePiece['azulEscuro'],
+              borderColor: appConfig.theme.colors.onePiece['azulEscuroTransparente'],
               borderRadius: '10px',
               flex: 1,
               minHeight: '240px',
